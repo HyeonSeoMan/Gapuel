830fcbe19884c0303f100876bf35a7a742e5e300
HyeonSeoJeong <hsjeong@inthecore.co.kr>
2021-03-11 19:53:12 +0900

    navigation & drawer

--- App.js
+++ App.js
@@ -1,66 +1,80 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import TutorialScreen from './screens/TutorialScreen';
+import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import IconWithBadge from './IconWithBadge';
 
-const App = () => {
-  const [todos, setTodos] = useState([]);
-  const addTodo = (text) => {
-    setTodos([
-      ...todos,
-      {id: Math.random().toString(), textValue: text, checked: false},
-    ]);
-  };
-  const onRemove = (id) => (e) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-  const onToggle = (id) => (e) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? {...todo, checked: !todo.checked} : todo,
-      ),
-    );
-  };
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.appTitle}>Hello Todolist</Text>
-      <View style={styles.card}>
-        <TodoInsert onAddTodo={addTodo} />
-        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-      </View>
-    </SafeAreaView>
-  );
+const HomeIconWithBadge = (props) => {
+  return <IconWithBadge {...props} badgeCount={3} />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3143e8',
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Chat: {
+      screen: ChatScreen,
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        header: null,
+      },
+    },
   },
-  appTitle: {
-    color: '#fff',
-    fontSize: 36,
-    marginTop: 30,
-    marginBottom: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    backgroundColor: '#3143e8',
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      swipeEnabled: true,
+      adaptive: true,
+      tabBarIcon: ({horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = 'ios-home';
+        } else if (routeName === 'Chat') {
+          iconName = 'ios-chatboxes';
+          IconComponent = HomeIconWithBadge;
+        } else if (routeName === 'Settings') {
+          iconName = 'ios-settings';
+        }
+
+        return (
+          <IconComponent
+            name={iconName}
+            size={horizontal ? 20 : 25}
+            color={tintColor}
+          />
+        );
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'white',
+      inactiveTintColor: 'gray',
+      style: {
+        backgroundColor: 'black',
+      },
+    },
   },
-  card: {
-    backgroundColor: '#fff',
-    flex: 1,
-    borderTopLeftRadius: 10, // to provide rounded corners
-    borderTopRightRadius: 10, // to provide rounded corners
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  input: {
-    padding: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: 1,
-    fontSize: 24,
-    marginLeft: 20,
+);
+
+const App = createStackNavigator({
+  screen: TutorialScreen,
+  TabNavigator: {
+    screen: TabNavigator,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#633689',
+      },
+      headerTintColor: '#FFFFFF',
+      title: 'React Native Tutorial',
+    },
   },
 });
-
-export default App;
+export default createAppContainer(App);
