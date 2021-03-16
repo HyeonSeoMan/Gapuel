de8d1c0671fbc45706faac1317b4fe6f75a980e5
HyeonSeoMan <rmsry123@gmail.com>
2021-03-16 20:00:05 +0900

    navigation 실 도입

--- App4.js
+++ App4.js
@@ -1,15 +1,50 @@
 import React from 'react';
-// import {TouchableOpacity, StyleSheet, TextInput, Text, View, Button} from 'react-native';
 import {createStore} from 'redux';
 import reducers from './store/reducers';
 import {Provider} from 'react-redux';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import SendMoney from './screens/SendMoney';
+import ReceiveMoney from './screens/ReceiveMoney';
+import ModalScreen from './screens/ModalScreen';
 import CounterListContainer from './containers/CounterListContainer';
 
+const MainStack = createStackNavigator(
+  {
+    SendMoney: {screen: SendMoney},
+    ReceiveMoney: {screen: ReceiveMoney},
+  },
+  {},
+);
+
+const RootStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainStack,
+    },
+    MyModal: {
+      screen: ModalScreen,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  },
+);
+
+const AppContainer = createAppContainer(RootStack);
+
 const store = createStore(reducers);
+
 const App4 = () => {
   return (
     <Provider store={store}>
-      <CounterListContainer />
+      {/* <CounterListContainer /> */}
+      <AppContainer
+        ref={(nav) => {
+          this.navigator = nav;
+        }}
+      />
     </Provider>
   );
 };
---
+++ screens/ModalScreen.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+  View,
+  ScrollView,
+} from 'react-native';
+
+const ModalScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+      <Button
+        onPress={() => props.navigation.goBack()}
+        title="Dismiss"
+      />
+    </View>
+  );
+};
+
+export default ModalScreen;
